597fecc877d7d8f3b0ea7a0b8486c72b25aa8d31
KikoKlyde <163093105+KikoKlyde@users.noreply.github.com>
2025-05-18 19:21:04 -0700

    Create CroptopiaBowlSpreads.java
    initial attempt at adding bowl contained spreads

---
+++ src/main/java/io/github/foundationgames/sandwichable/compat/CroptopiaBowlSpreads.java
@@ -0,0 +1,30 @@
+package io.github.foundationgames.sandwichable.compat;
+
+import io.github.foundationgames.sandwichable.items.spread.SpreadType;
+import io.github.foundationgames.sandwichable.util.SpreadRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+
+import java.util.Optional;
+
+public class CroptopiaBowlSpreads {
+    public static void init() {
+        addBowlSpread("vanilla_ice_cream", 0xe8d7bd);
+    }
+
+    private static void addBowlSpread(String itemId, int color) {
+        Optional<Item> oitem = Registries.ITEM.getOrEmpty(new Identifier("croptopia", itemId));
+        if(oitem.isPresent()) {
+            Item item = oitem.get();
+            int h = 0;
+            float s = 0;
+            if(item.isFood()) {
+                h = item.getFoodComponent().getHunger();
+                s = item.getFoodComponent().getSaturationModifier();
+            }
+            SpreadRegistry.INSTANCE.register(itemId, new SpreadType(h, s, color, item, Items.BOWL));
+        }
+    }
+}
