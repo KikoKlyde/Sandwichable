78239e609e94812b0c8dca788fe8979cce515ba4
KikoKlyde <163093105+KikoKlyde@users.noreply.github.com>
2025-05-22 22:50:47 -0700

    Update Sandwichable.java
    initialize bowl spreads (actual)

--- src/main/java/io/github/foundationgames/sandwichable/Sandwichable.java
+++ src/main/java/io/github/foundationgames/sandwichable/Sandwichable.java
@@ -196,6 +196,7 @@ public class Sandwichable implements ModInitializer {
 
         if(FabricLoader.getInstance().isModLoaded("croptopia")) {
             CroptopiaBottleSpreads.init();
+            CroptopiaBowlSpread.init();
         }
 
         CommonTags.init();
